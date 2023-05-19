7246dff4398c38d0c7343acc83f1d566babea210
Muhammad Talha <talhahanfsd@gmail.com>
2023-05-19 23:00:41 +0500

    Add Screens

--- App.jsx
+++ App.jsx
@@ -1,21 +1,38 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from './src/pages/Home';
+import LoginScreen from './src/pages/LoginScreen';
+import RegistrationScreen from './src/pages/RegistrationScreen';
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
   return (
     <View style={{flex: 1}}>
       <StatusBar translucent backgroundColor="transparent" />
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{headerShown: false}}
-          initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} />
+          initialRouteName={user ? 'Home' : 'Login'}>
+          {user ? (
+            <Stack.Screen name="Home">
+              {props => <Home {...props} extraData={user} />}
+            </Stack.Screen>
+          ) : (
+            <>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen
+                name="Registration"
+                component={RegistrationScreen}
+              />
+            </>
+          )}
         </Stack.Navigator>
       </NavigationContainer>
     </View>
@@ -23,5 +40,3 @@ const App = () => {
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
