5a9f542d70c2ed5f2f50c814453b71ab725060ed
Muhammad Talha <talhahanfsd@gmail.com>
2023-05-24 23:45:59 +0500

    Add Toast & Separate Nav Component

--- App.jsx
+++ App.jsx
@@ -1,47 +1,24 @@
 import 'react-native-gesture-handler';
-import React, {useState} from 'react';
+import React from 'react';
 import {View, StatusBar} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import Home from './src/pages/Home';
-import LoginScreen from './src/pages/LoginScreen';
-import RegistrationScreen from './src/pages/RegistrationScreen';
+
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import Toast from 'react-native-toast-message';
 
 import {store, persistor} from './src/store/store';
-const Stack = createStackNavigator();
+import Navigation from './src/pages/Navigation';
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState(null);
-
   return (
     <View style={{flex: 1}}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <StatusBar translucent backgroundColor="transparent" />
-          <NavigationContainer>
-            <Stack.Navigator
-              screenOptions={{headerShown: false}}
-              initialRouteName={user ? 'Home' : 'Login'}>
-              {user ? (
-                <Stack.Screen name="Home">
-                  {props => <Home {...props} extraData={user} />}
-                </Stack.Screen>
-              ) : (
-                <>
-                  <Stack.Screen name="Login" component={LoginScreen} />
-                  <Stack.Screen
-                    name="Registration"
-                    component={RegistrationScreen}
-                  />
-                </>
-              )}
-            </Stack.Navigator>
-          </NavigationContainer>
+          <Navigation />
         </PersistGate>
       </Provider>
+      <Toast />
     </View>
   );
 };
