9af7f76ee9517da3c96a145832e9304143e1ec8a
Muhammad Talha <talhahanfsd@gmail.com>
2023-05-26 23:24:48 +0500

    Design & Add Form Validation to Login Scr

--- src/pages/LoginScreen.jsx
+++ src/pages/LoginScreen.jsx
@@ -1,25 +1,111 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
-  Image,
   Text,
   TextInput,
   TouchableOpacity,
   View,
+  StatusBar,
   StyleSheet,
+  ImageBackground,
+  Keyboard,
 } from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
-import {auth} from '../firebase/config';
-import {signInWithEmailAndPassword} from 'firebase/auth';
-import actionCreators from '../store/actionCreators';
-import {useSelector, useDispatch} from 'react-redux';
 import Toast from 'react-native-toast-message';
+
+import {auth} from '../firebase/config';
+import {
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+} from 'firebase/auth';
+
+import actionCreators from '../store/actionCreators';
+import {useDispatch} from 'react-redux';
+
+import bgImg from '../assets/images/bg.png';
+import Google from '../assets/svgs/Google';
+import Facebook from '../assets/svgs/Facebook';
+import Apple from '../assets/svgs/Apple';
+
+import colors from '../constants/colors';
+import ratios from '../constants/ratios';
+import fonts from '../constants/fonts';
 
 export default function LoginScreen({navigation}) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isKeyboardActive, setKeyboardActive] = useState(false);
 
-  const state = useSelector(state => state);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => setKeyboardActive(true),
+    );
+
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => setKeyboardActive(false),
+    );
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+
+  const validateEmail = email => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleForgetPassword = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email) {
+      Toast.show({
+        type: 'error',
+        text1: `Enter Email to get reset link`,
+      });
+    } else if (emailRegex.test(email)) {
+      sendPasswordResetEmail(auth, email)
+        .then(() => {
+          Toast.show({
+            type: 'success',
+            text1: `Reset link sent to email`,
+          });
+        })
+        .catch(error => {
+          let code = error.code.split('/');
+          Toast.show({
+            type: 'error',
+            text1: `${code[0]} error ⚠️`,
+            text2: `${code[1].replace(/-/g, ' ')}`,
+          });
+        });
+    } else {
+      Toast.show({
+        type: 'error',
+        text1: `Enter Valid Email to get reset link`,
+      });
+    }
+  };
+  const handleSubmit = () => {
+    if (!email || !password) {
+      Toast.show({
+        type: 'error',
+        text1: 'Please fill in all fields',
+      });
+    } else if (!validateEmail(email)) {
+      Toast.show({
+        type: 'error',
+        text1: 'Please enter a valid email address',
+      });
+    } else if (password.length < 8) {
+      Toast.show({
+        type: 'error',
+        text1: 'Password should be at least 8 characters long',
+      });
+    } else onLoginPress();
+  };
 
   const onFooterLinkPress = () => {
     navigation.navigate('Registration');
@@ -53,105 +139,171 @@ export default function LoginScreen({navigation}) {
   };
 
   return (
-    <View style={styles.container}>
-      <KeyboardAwareScrollView
-        style={{flex: 1, width: '100%'}}
-        keyboardShouldPersistTaps="always">
-        <TextInput
-          style={styles.input}
-          placeholder="E-mail"
-          placeholderTextColor="#aaaaaa"
-          onChangeText={text => setEmail(text)}
-          value={email}
-          underlineColorAndroid="transparent"
-          autoCapitalize="none"
-        />
-        <TextInput
-          style={styles.input}
-          placeholderTextColor="#aaaaaa"
-          secureTextEntry
-          placeholder="Password"
-          onChangeText={text => setPassword(text)}
-          value={password}
-          underlineColorAndroid="transparent"
-          autoCapitalize="none"
-        />
-        <TouchableOpacity style={styles.button} onPress={() => onLoginPress()}>
-          <Text style={styles.buttonTitle}>
-            {state.auth.uid !== '' ? 'Logged In' : 'Log In'}
-          </Text>
-        </TouchableOpacity>
-        <View style={styles.footerView}>
-          <Text style={styles.footerText}>
-            Don't have an account?{' '}
-            <Text onPress={onFooterLinkPress} style={styles.footerLink}>
-              Sign up
-            </Text>
-          </Text>
-        </View>
-        {Object.keys(state.todos).map(key => (
-          <View key={key} onTouchEnd={() => removeTask(key)}>
-            <Text>{state.todos[key].title}</Text>
-            <Text>desc {JSON.stringify(state.todos[key].description)}</Text>
+    <ImageBackground
+      source={!isKeyboardActive ? bgImg : ''}
+      resizeMode="cover"
+      style={styles.bg}>
+      <KeyboardAwareScrollView keyboardShouldPersistTaps="always">
+        <View style={styles.parent}>
+          <Text style={styles.mainHeading}>Login</Text>
+          <View style={styles.inputPar}>
+            <View style={styles.textBoxPar1}>
+              <Text style={styles.textBoxText}>Email</Text>
+              <TextInput
+                style={styles.textBox}
+                placeholder="Email"
+                placeholderTextColor={colors.beige}
+                onChangeText={text => setEmail(text)}
+                value={email}
+                underlineColorAndroid="transparent"
+                autoCapitalize="none"
+                autoFocus={true}
+                inputMode="email"
+              />
+            </View>
+            <View style={styles.textBoxPar2}>
+              <Text style={styles.textBoxText}>Password</Text>
+              <TextInput
+                style={styles.textBox}
+                placeholderTextColor={colors.beige}
+                secureTextEntry
+                placeholder="Password"
+                onChangeText={text => setPassword(text)}
+                value={password}
+                underlineColorAndroid="transparent"
+                autoCapitalize="none"
+              />
+            </View>
+            <TouchableOpacity onPress={handleForgetPassword}>
+              <Text style={styles.forgPass}>Forget Password?</Text>
+            </TouchableOpacity>
           </View>
-        ))}
+          <View style={styles.logoPar}>
+            <TouchableOpacity style={styles.logo}>
+              <Google />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.logo}>
+              <Facebook />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.logo}>
+              <Apple />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.parCTA}>
+            <View style={styles.secCtaPar}>
+              <Text style={styles.secCta}>New Here? </Text>
+              <TouchableOpacity onPress={onFooterLinkPress}>
+                <Text style={styles.secCtaBtn}>Register</Text>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity onPress={handleSubmit} style={styles.mainCtaPar}>
+              <Text style={styles.mainCta}>Login</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </KeyboardAwareScrollView>
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  bg: {
+    position: 'relative',
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+    backgroundColor: colors.pink,
   },
-  title: {},
+  parent: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    marginLeft: ratios.widthPixel(20),
+    marginRight: ratios.widthPixel(32),
+    marginBottom: ratios.widthPixel(20),
+  },
+  mainHeading: {
+    fontFamily: fonts.bol,
+    fontSize: ratios.fontPixel(37),
+    color: colors.beige,
+    paddingBottom: ratios.widthPixel(24),
+  },
+  textBoxPar1: {
+    marginBottom: ratios.widthPixel(20),
+  },
+  textBoxPar2: {
+    marginBottom: ratios.widthPixel(12),
+  },
+  textBox: {
+    borderWidth: ratios.widthPixel(1.5),
+    borderColor: colors.beige,
+    borderRadius: ratios.widthPixel(10),
+    paddingHorizontal: ratios.widthPixel(10),
+    paddingVertical: ratios.heightPixel(6),
+    color: colors.beige,
+  },
+  textBoxText: {
+    fontFamily: fonts.reg,
+    fontSize: ratios.fontPixel(15),
+    color: colors.beige,
+    paddingBottom: ratios.heightPixel(4),
+  },
+  forgPass: {
+    fontFamily: fonts.bol,
+    fontSize: ratios.fontPixel(15),
+    color: colors.beige,
+    textAlign: 'right',
+  },
+  inputPar: {
+    marginRight: ratios.widthPixel(48),
+  },
   logo: {
-    flex: 1,
-    height: 120,
-    width: 90,
-    alignSelf: 'center',
-    margin: 30,
+    padding: ratios.widthPixel(8),
+    backgroundColor: colors.beige,
+    borderRadius: ratios.widthPixel(5),
+    elevation: ratios.widthPixel(6),
+    shadowColor: '#000',
+    shadowOffset: {width: ratios.widthPixel(3), height: ratios.widthPixel(2)},
+    shadowOpacity: 0.25,
+    shadowRadius: ratios.widthPixel(20),
   },
-  input: {
-    height: 48,
-    borderRadius: 5,
-    overflow: 'hidden',
-    backgroundColor: 'white',
-    marginTop: 10,
-    marginBottom: 10,
-    marginLeft: 30,
-    marginRight: 30,
-    paddingLeft: 16,
+  logoPar: {
+    flexDirection: 'row',
+    gap: ratios.widthPixel(16),
   },
-  button: {
-    backgroundColor: '#788eec',
-    marginLeft: 30,
-    marginRight: 30,
-    marginTop: 20,
-    height: 48,
-    borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
+  parCTA: {
+    marginTop: ratios.widthPixel(40),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
-  buttonTitle: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
+  mainCtaPar: {
+    backgroundColor: colors.beige,
+    paddingHorizontal: ratios.widthPixel(42),
+    paddingVertical: ratios.widthPixel(12),
+    borderRadius: ratios.widthPixel(7),
+    elevation: ratios.widthPixel(6),
+    shadowColor: '#000',
+    shadowOffset: {width: ratios.widthPixel(3), height: ratios.widthPixel(2)},
+    shadowOpacity: 0.25,
+    shadowRadius: ratios.widthPixel(20),
   },
-  footerView: {
-    flex: 1,
-    alignItems: 'center',
-    marginTop: 20,
+  mainCta: {
+    fontFamily: fonts.reg,
+    fontSize: ratios.fontPixel(24),
+    color: colors.brown,
   },
-  footerText: {
-    fontSize: 16,
-    color: '#2e2e2d',
+  secCtaPar: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    marginBottom: ratios.widthPixel(4),
   },
-  footerLink: {
-    color: '#788eec',
-    fontWeight: 'bold',
-    fontSize: 16,
+  secCta: {
+    fontFamily: fonts.reg,
+    fontSize: ratios.fontPixel(17),
+    color: colors.beige,
+  },
+  secCtaBtn: {
+    fontFamily: fonts.bol,
+    fontSize: ratios.fontPixel(17),
+    color: colors.beige,
   },
 });
